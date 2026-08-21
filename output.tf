e77fd284fc3f7a006bc83c642b2e2e4cc54069d9
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-08-22 01:40:28 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,4 @@ output "aws_iam_instance_profile" {
 output "aws_iam_role" {
   value = module.instance-profile.aws_iam_role
 }
+=TEXTJOIN(CHAR(10),TRUE,UNIQUE(FILTER($D$2:$D$10000,$C$2:$C$10000=C2,"")))
